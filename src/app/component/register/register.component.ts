ef2d04e77f09ec208093ea4d3a40e7fe2a1d9ba9
DinaElmasry <dinaelmasry666@gmail.com>
2023-04-17 00:26:36 +0200

    Add Registration

--- src/app/component/register/register.component.ts
+++ src/app/component/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { RegisterService } from 'src/app/Services/register.service';
 
 @Component({
   selector: 'app-register',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  constructor(private service:RegisterService, private router: Router){}
+
+  Register(name:any, email:any, phone:any , bankAccount:any , password:any){
+    if (name && email && phone  && bankAccount && password)
+    { // Check if all required fields have data
+      let newReg = {name , email , phone , bankAccount , password};
+      this.service.AddNewRegister(newReg).subscribe();
+      // Navigate to home component after adding the new Register
+      this.router.navigate(['/']);
+    }
+  }
 }
