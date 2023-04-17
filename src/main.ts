6f2a0657c9d3e44c043f284dfe19c0f107926fe0
Eman Mohammed <emanmohammedhussin@gmail.com>
2023-04-17 06:40:41 +0200

    add error component html & edit home - header components

--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,27 @@ $(window).scroll(() => {
   const scrollTop = $(window)?.scrollTop?.();
   if (scrollTop && scrollTop > 45) {
     $('.navbar').addClass('sticky-top shadow-sm');
+    $('.back-to-top').css('display', 'block');
   } else {
     $('.navbar').removeClass('sticky-top shadow-sm');
+    $('.back-to-top').css('display', 'none');
+  }
+});
+$('a.back-to-top').click(function(event) {
+  event.preventDefault(); // Prevent the default behavior of the link
+  var target = $(this).attr('href'); // Get the href attribute of the link
+  if (target === '#' || !target) { // If the href is empty or "#"
+    $('html, body').animate({scrollTop: 0}, 800); // Scroll to the top of the page
+  } else { // If the href is not empty or "#"
+    $('html, body').animate({scrollTop: 0}, 800); // Scroll to the top of the page
   }
 });
 
 // Dropdown on mouse hover
-const $dropdown = $(".dropdown");
-const $dropdownToggle = $(".dropdown-toggle");
-const $dropdownMenu = $(".dropdown-menu");
-const showClass = "show";
+// const $dropdown = $(".dropdown");
+// const $dropdownToggle = $(".dropdown-toggle");
+// const $dropdownMenu = $(".dropdown-menu");
+// const showClass = "show";
 
 // $(window).on("load resize", function() {
 //     if (this.matchMedia("(min-width: 992px)").matches) {
