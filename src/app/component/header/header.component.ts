991884dbbc1cca62fd8f1781f9d4d67bf2c74213
Eman Mohammed <76956637+eman120@users.noreply.github.com>
2023-05-02 13:41:18 +0300

    edit general header to fit customer log out

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component} from '@angular/core';
+import { Component, Input} from '@angular/core';
+import { AuthService } from 'src/app/Services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -6,7 +7,11 @@ import { Component} from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  
-  
+  @Input() user: string ="";
+  constructor (private auth: AuthService) {}
+
+  logout(){
+    this.auth.logout();
+  }
 }
 
