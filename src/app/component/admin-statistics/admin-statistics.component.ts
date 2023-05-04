e0892007644e3c05e866842a518e0ed738f239ee
Mahmoud Sherif <127897127+MahmoudSherif74@users.noreply.github.com>
2023-05-04 20:08:03 +0300

    updated/adminstat

--- src/app/component/admin-statistics/admin-statistics.component.ts
+++ src/app/component/admin-statistics/admin-statistics.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { error } from 'jquery';
+
 
 @Component({
   selector: 'app-admin-statistics',
@@ -6,5 +9,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin-statistics.component.css']
 })
 export class AdminStatisticsComponent {
+  obj:any
+  viewsCount: any;
+  Completed:any;
+  TopTourRevenueName:any;
+  Revenue:any;
+  RefundRate:any;
+  Name:any;
+  Names:any
+  constructor(private http: HttpClient) { }
+  ngOnInit() {
+    this.http.get('https://localhost:44362/api/DashBoard/websiteviews').subscribe(result => {
+      this.viewsCount = result;
+    }, error => console.error(error));
+    this.http.get('https://localhost:44362/api/DashBoard/CompletedTours').subscribe(result=>{
+      this.Completed=result
+    },error=>console.error(error));
+     this.http.get('https://localhost:44362/api/DashBoard/TopTourRevenueName').subscribe(result=>{
+     this.obj=result
+     this.TopTourRevenueName=this.obj.tourName
+     console.log(result)
+     },error=>console.error(error));
+     this.http.get('https://localhost:44362/api/DashBoard/TopTourRevenueMoney').subscribe(result=>{
+       this.Revenue=result
+    },error=>console.error(error));
+     this.http.get('https://localhost:44362/api/DashBoard/refundRate').subscribe(result=>{
+       this.RefundRate=result
+     },error=>console.error(error));
+
+  }
+
+
+  onSearchSubmit() {
+    this.http.get('https://localhost:44362/api/DashBoard/TourGuideName?Name='+ this.Name).subscribe(
+      (result: any) => {
+        this.Names = result;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+
 
 }
