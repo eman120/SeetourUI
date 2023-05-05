f2b77e01277e677f87115b03c5ff327817cf5b11
Eman Mohammed <76956637+eman120@users.noreply.github.com>
2023-05-06 02:43:53 +0300

    customer & tourguide tour details

--- src/app/component/tour-card/tour-card.component.ts
+++ src/app/component/tour-card/tour-card.component.ts
@@ -1,16 +1,29 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { TourCard } from 'src/app/Interfaces/tour-card';
+import { AuthService } from 'src/app/Services/auth.service';
 
 @Component({
   selector: 'app-tour-card',
   templateUrl: './tour-card.component.html',
   styleUrls: ['./tour-card.component.css']
 })
-export class TourCardComponent {
+export class TourCardComponent implements  OnInit {
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+  ) { }
+  user :any;
   @Input() tour: TourCard | undefined;
-
+  
+  ngOnInit(): void {
+    this.user = this.authService.getInterface();
+  }
   get tourLink(): string {
-    return this.tour ? `/tour/${this.tour.id}` : '#';
+    if(this.user === 'TourGuide'){
+      return this.tour ? `/tour/${this.tour.id}` : '#';
+    }
+    return this.tour ? `/custdetails/${this.tour.id}` : '#';
   }
   get tourguideLink(): string {
     return this.tour? `/tourguide/${this.tour.tourGuideId}` : '#';
