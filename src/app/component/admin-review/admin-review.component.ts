caa30fbd6b435dccc38f0afd7e48b26c16dc7193
Mahmoud Sherif <127897127+MahmoudSherif74@users.noreply.github.com>
2023-05-06 08:00:29 +0300

    Admin Review/Addition

--- src/app/component/admin-review/admin-review.component.ts
+++ src/app/component/admin-review/admin-review.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-admin-review',
   templateUrl: './admin-review.component.html',
   styleUrls: ['./admin-review.component.css']
 })
-export class AdminReviewComponent {
+export class AdminReviewComponent implements OnInit {
+  reviews: any;
+  tourGuideName: any;
+  tourLink: any;
+  FullName:any;
+  Comment:any;
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get<any[]>('https://localhost:44362/api/Review').subscribe(
+      data => {
+        this.reviews = data;
+      },
+      error => {
+        console.error(error);
+      }
+    );
+
+
+  }
+  deleteReview(id: any) {
+    this.http.delete(`https://localhost:44362/api/Review?id=${id}`).subscribe(() => {
+      this.http.get<any[]>('https://localhost:44362/api/Review').subscribe(
+        data => {
+          this.reviews = data;
+        },
+        error => {
+          console.error(error);
+          alert('Failed to fetch reviews. Please refresh the page.');
+        }
+      );
+    });
+  }
+
+
+
 
 }
